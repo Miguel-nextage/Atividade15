343da14b6e23c6a148cf06e6dc227b418e0553c6
Mings <pmiguelhkss@gmail.com>
2024-09-16 08:40:00 -0300

    calculadora

--- atividade15.py
+++ atividade15.py
@@ -1,22 +1,24 @@
 # Crie um programa que receba dois números e uma operação (soma, subtração, multiplicação ou divisão) e execute a operação desejada.
 
-N1, N2 = float(input("Seu primeiro numero")), float(input("Seu segundo numero"))
-op = input("Qual operação deseja?")
+N1 = float(input("Seu primeiro numero"))
+op = input("Qual operação deseja")
+N2 = float(input("Seu segundo numero"))
+
 result = 0
 
-if op == "soma":
+if op == "soma" or "+":
     result = N1 + N2
     print(F"Seu resultado é {result}") 
 
-elif op == "subtração":
+elif op == "subtração" or "-":
     result = N1 - N2
     print(F"Seu resultado é {result}") 
 
-elif op == "multiplicação":
+elif op == "multiplicação" or "*":
     result = N1 * N2
     print(F"Seu resultado é {result}") 
 
-elif op == "divisão":
+elif op == "divisão" or "/":
     result = N1 / N2
     print(F"Seu resultado é {result}") 
 
